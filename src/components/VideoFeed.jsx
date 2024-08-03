106311bfb6810faab2bf07194c16e1e1e7e484e8
sarojbist <bistsaroj24@gmail.com>
2024-08-03 19:30:26 +0530

    jpt

--- src/components/VideoFeed.jsx
+++ src/components/VideoFeed.jsx
@@ -6,6 +6,7 @@ const VideoFeed = ({ isHamOpen, category, setCategory }) => {
     const { categoryId, videoId } = useParams();
     return (
 
+        
         <>
             {/* <LeftNavbar isHamOpen={isHamOpen} category={category} setCategory={setCategory} /> */}
             <div className="grid grid-cols-[60vw_auto] gap-[2.5vw] px-[5vw]">
